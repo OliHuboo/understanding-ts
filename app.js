2461cf8d1ac7787b7a7f60ca5b0be2fc5972f8df
Oward1 <oliver.ward94@gmail.com>
2022-10-21 16:39:52 +0100

    Update

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result:' + num);
 }
-printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
